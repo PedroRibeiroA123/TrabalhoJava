a5712a0cf7359ee3cfb68b6f1f65c495ccca8c89
Pedro Ribeiro <pedro.ribeiro1@ufu.br>
2023-05-15 16:56:38 -0300

    .

--- trabalho/src/Funcionario.java
+++ trabalho/src/Funcionario.java
@@ -11,4 +11,70 @@ public class Funcionario {
     private double salario;
     private int anoIngresso;
     private Date dataNascimento;
+    public String getEndereco() {
+        return endereco;
+    }
+    public String getCPF() {
+        return CPF;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public int getAnoIngresso() {
+        return anoIngresso;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+    public String getNroCT() {
+        return nroCT;
+    }
+    public String getRG() {
+        return RG;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public void setAnoIngresso(int anoIngresso) {
+        this.anoIngresso = anoIngresso;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+    public void setNroCT(String nroCT) {
+        this.nroCT = nroCT;
+    }
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 }
